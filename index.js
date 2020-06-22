8db189b7701b09f4c4838cc3f2f6d11e305feb90
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-22 10:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,11 @@ class Polygon{
 class Triangle extends Polygon{
   get isValid(){
     let side1 = this.sides[0];
+    let side2 = this.sides[1];
+    let side3 = this.sides[2];
+    
+    if(side1 + side2 > side3 && side3 + side2 > side1 && side1 + side3 > side2){
+      return true;
+    }
   }
 }
